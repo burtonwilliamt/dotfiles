eada79b54d3c1b98bb0e7fc41689aab4bd9a3789
William Burton <13026711+burtonwilliamt@users.noreply.github.com>
2025-02-08 22:19:53 -0500

    new machine setup scripts

---
+++ scripts/new_machine.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -eux
+
+handle_exit() {
+    echo ''
+    echo 'Ctrl-C, exiting.'
+    exit
+}
+
+trap 'handle_exit' INT
+
+echo "Updating and upgrading..."
+apt update
+apt dist-upgrade -y
+read -p "Install essential tools [y/N? " REPLY
+if [[ $REPLY =~ ^[yY]$ ]]
+then
+    apt install sudo vim tmux git
+fi
+read -p "Name for new user (leave empty to skip): " REPLY
+if ! [[ $REPLY =~ ^\s*$ ]]
+then
+    NEWUSER=$REPLY
+    echo "Creating user $NEWUSER."
+    useradd -m -U -s /bin/bash $NEWUSER
+    echo "Set password for $NEWUSER"
+    passwd $NEWUSER
+    apt install -y sudo
+    usermod -aG sudo $NEWUSER
+    su - $NEWUSER
+fi
---
+++ scripts/new_user.sh
@@ -0,0 +1,41 @@
+#!bin/bash
+set -eu
+
+handle_exit() {
+    echo ''
+    echo 'Ctrl-C, exiting.'
+    exit
+}
+
+trap 'handle_exit' INT
+
+cd ~
+read -p "Do you want to install dotfiles? [Y/n]" -n 1 REPLY
+if [[ $REPLY =~ ^\s*$ ]] || [[ $REPLY =~ ^[yY]$ ]]
+then
+    mkdir -p ~/github
+    cd ~/github
+    # TODO: Check if the dotfiles already exist.
+    git clone https://github.com/burtonwilliamt/dotfiles.git
+    cd dotfiles
+    sh ./setup.sh
+fi
+
+read -p "Do you want to setup docker? [Y/n]" -n 1 REPLY
+if [[ $REPLY =~ ^\s*$ ]] || [[ $REPLY =~ ^[yY]$ ]]
+then
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get -y install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo docker run hello-world
+fi
